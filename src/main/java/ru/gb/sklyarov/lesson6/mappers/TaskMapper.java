602e3fcf4432ac1e173af2ee396ccf9d58d148a5
Evgeniy Sklyarov <skevni@gmail.com>
2021-12-26 00:17:31 +0300

    hw6
    реализация DataMapper и Identity Map

--- src/main/java/ru/gb/sklyarov/lesson6/mappers/TaskMapper.java
+++ src/main/java/ru/gb/sklyarov/lesson6/mappers/TaskMapper.java
@@ -1,5 +1,7 @@
 package ru.gb.sklyarov.lesson6.mappers;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import ru.gb.sklyarov.lesson3.model.Category;
 import ru.gb.sklyarov.lesson3.model.Organization;
@@ -8,26 +10,31 @@ import ru.gb.sklyarov.lesson3.model.Task;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class TaskMapper implements RowMapper<Task> {
+public class TaskMapper implements ResultSetExtractor<Map<Long,Task>> {
 
     @Override
-    public Task mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Task task = new Task();
-        List<Category> categories = new ArrayList<>();
-        List<Organization> organizations = new ArrayList<>();
-        task.setId(rs.getLong("id"));
-        task.setTaskName(rs.getString("name"));
-        task.setComment(rs.getString("comment"));
+    public Map<Long, Task> extractData(ResultSet rs) throws SQLException, DataAccessException {
+        Map<Long, Task> tasks = new HashMap<>();
 
         while (rs.next()) {
-            categories.add(new Category(rs.getInt("cat_id"), rs.getString("cat_name")));
-            organizations.add(new Organization(rs.getInt("org_id"), rs.getString("org_name")));
+            long key = rs.getLong("id");
+            if (!tasks.containsKey(key)) {
+                Task task = new Task();
+                task.setId(key);
+                task.setTaskName(rs.getString("name"));
+                task.setComment(rs.getString("comment"));
+                task.setCategories(new ArrayList<>());
+                task.setOrganizations(new ArrayList<>());
+                tasks.put(key, task);
+            } else {
+                tasks.get(key).getCategories().add(new Category(rs.getInt("cat_id"), rs.getString("cat_name")));
+                tasks.get(key).getOrganizations().add(new Organization(rs.getInt("org_id"), rs.getString("org_name")));
+            }
         }
-        task.setCategories(categories);
-        task.setOrganizations(organizations);
 
-        return task;
+        return tasks;
     }
 }
--- src/main/java/ru/gb/sklyarov/lesson6/repositories/TaskRepository.java
+++ src/main/java/ru/gb/sklyarov/lesson6/repositories/TaskRepository.java
@@ -1,14 +1,19 @@
 package ru.gb.sklyarov.lesson6.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.gb.sklyarov.lesson3.model.Task;
 import ru.gb.sklyarov.lesson6.mappers.TaskMapper;
 
+import java.util.Objects;
 import java.util.WeakHashMap;
 
 public class TaskRepository {
     private final JdbcTemplate jdbcTemplate;
     private final WeakHashMap<Long, Task> identityMap = new WeakHashMap<>();
+
+    @Autowired
+    private TaskMapper taskMapper;
 
     public TaskRepository() {
         this.jdbcTemplate = new JdbcTemplate(DataSourcePostgresql.getDataSource());
@@ -17,11 +22,11 @@ public class TaskRepository {
     public Task getTaskById(Long id) {
         Task task = identityMap.get(id);
         if (task == null) {
-            task = jdbcTemplate.queryForObject("select t.id, t.name, t.comment, o.id org_id, o.name org_name, tc.id cat_id, tc.name cat_name " +
+            task = Objects.requireNonNull(jdbcTemplate.query("select t.id, t.name, t.comment, o.id org_id, o.name org_name, tc.id cat_id, tc.name cat_name " +
                             "from tbl_task t inner join task_organization o on o.task_id = t.id" +
                             "inner join task_category tc on tc.task_id = t.id" +
                             "where t.id = ?"
-                    , new TaskMapper(), id);
+                    , taskMapper, id)).get(id);
             if (task != null) {
                 identityMap.put(id, task);
             }
@@ -31,7 +36,7 @@ public class TaskRepository {
 
     public void save(Task task) {
 
-        if (jdbcTemplate.update("delete from tbl_task where id = ?", task.getId()) != 0){
+        if (jdbcTemplate.update("delete from tbl_task where id = ?", task.getId()) != 0) {
             identityMap.remove(task.getId());
         }
     }
